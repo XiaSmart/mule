d9e1cad14cfdae18ddcc0ff4b9c1fc6abc4d2a78
Pablo La Greca <pablo.lagreca@mulesoft.com>
2016-06-07 16:53:48 -0300

    adding changes to make it work for now

--- modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/config/dsl/config/ConfigurationProviderObjectFactory.java
+++ modules/extensions-spring-support/src/main/java/org/mule/runtime/module/extension/internal/config/dsl/config/ConfigurationProviderObjectFactory.java
@@ -11,6 +11,12 @@ import static java.util.Optional.ofNullable;
 import static org.mule.runtime.core.api.config.MuleProperties.OBJECT_TIME_SUPPLIER;
 import org.mule.runtime.api.connection.ConnectionProvider;
 import org.mule.runtime.core.api.MuleContext;
+import org.mule.runtime.core.api.MuleException;
+import org.mule.runtime.core.api.MuleRuntimeException;
+import org.mule.runtime.core.api.lifecycle.Initialisable;
+import org.mule.runtime.core.api.lifecycle.InitialisationException;
+import org.mule.runtime.core.api.lifecycle.Lifecycle;
+import org.mule.runtime.core.api.lifecycle.LifecycleUtils;
 import org.mule.runtime.core.time.TimeSupplier;
 import org.mule.runtime.extension.api.introspection.config.RuntimeConfigurationModel;
 import org.mule.runtime.extension.api.runtime.ConfigurationProvider;
@@ -24,8 +30,13 @@ import org.mule.runtime.module.extension.internal.runtime.resolver.ValueResolver
 
 import java.util.Optional;
 
-class ConfigurationProviderObjectFactory extends AbstractExtensionObjectFactory<ConfigurationProvider<Object>>
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class ConfigurationProviderObjectFactory extends AbstractExtensionObjectFactory<ConfigurationProvider<Object>> implements Lifecycle
 {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigurationProviderObjectFactory.class);
 
     private final String name;
     private final RuntimeConfigurationModel configurationModel;
@@ -34,6 +45,7 @@ class ConfigurationProviderObjectFactory extends AbstractExtensionObjectFactory<
 
     private DynamicConfigPolicy dynamicConfigPolicy;
     private Optional<ValueResolver<ConnectionProvider>> connectionProviderResolver = empty();
+    private ConfigurationProvider<Object> instance;
 
     ConfigurationProviderObjectFactory(String name,
                                        RuntimeConfigurationModel configurationModel,
@@ -46,6 +58,11 @@ class ConfigurationProviderObjectFactory extends AbstractExtensionObjectFactory<
 
     @Override
     public ConfigurationProvider<Object> getObject() throws Exception
+    {
+        return instance;
+    }
+
+    private ConfigurationProvider<Object> createInnerInstance()
     {
         ResolverSet resolverSet = getParametersAsResolverSet();
         final ValueResolver<ConnectionProvider> connectionProviderResolver = getConnectionProviderResolver();
@@ -106,5 +123,39 @@ class ConfigurationProviderObjectFactory extends AbstractExtensionObjectFactory<
     public void setConnectionProviderResolver(ConnectionProviderResolver connectionProviderResolver)
     {
         this.connectionProviderResolver = ofNullable(connectionProviderResolver);
+    }
+
+    @Override
+    public void initialise() throws InitialisationException
+    {
+        instance = createInnerInstance();
+        try
+        {
+            muleContext.getInjector().inject(instance);
+        }
+        catch (MuleException e)
+        {
+            throw new MuleRuntimeException(e);
+        }
+        LifecycleUtils.initialiseIfNeeded(instance);
+    }
+
+
+    @Override
+    public void dispose()
+    {
+        LifecycleUtils.disposeIfNeeded(instance, logger);
+    }
+
+    @Override
+    public void start() throws MuleException
+    {
+        LifecycleUtils.startIfNeeded(instance);
+    }
+
+    @Override
+    public void stop() throws MuleException
+    {
+        LifecycleUtils.stopIfNeeded(instance);
     }
 }
